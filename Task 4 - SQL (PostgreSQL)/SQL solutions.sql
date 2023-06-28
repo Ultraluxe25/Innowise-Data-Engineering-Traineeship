d0d0eb291606789dad3fa2c39782b4947f0fdaa0
Alexander Kapturov <ultraluxe25@mail.ru>
2023-06-28 11:08:50 +0300

    complited 6th query from 3rd Task

--- Task 4 - SQL (PostgreSQL)/SQL solutions.sql
+++ Task 4 - SQL (PostgreSQL)/SQL solutions.sql
@@ -108,3 +108,27 @@ FROM
 	) AS temporary_table  -- subquery result table
 WHERE
 	appearance_rank < 4;
+
+
+-- 6. Display cities with the number of active and inactive customers (active — customer.active = 1). 
+-- Sort by the number of inactive clients in descending order.
+SELECT
+	city.city,
+	COUNT(CASE
+			WHEN customer.active = 1
+				THEN 1
+		END) AS active_customers,
+	COUNT(CASE
+			WHEN customer.active = 0
+				THEN 1
+		END) AS inactive_customers,
+FROM
+	city
+	INNER JOIN address ON
+		city.city_id = address.city_id
+	INNER JOIN customer ON
+		address.address_id = customer.address_id
+GROUP BY
+	city.city
+ORDER BY
+	inactive_customers DESC;
